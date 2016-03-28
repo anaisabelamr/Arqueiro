74a7a15b0cdf6309e1ed3511dcca5132515c10d0
Ana Isabela Manito Ramos <aisabelamr@gmail.com>
2016-03-28 15:07:51 -0300

    Tentativa de DynamicCast

--- Arqueiro/main.cpp
+++ Arqueiro/main.cpp
@@ -22,7 +22,7 @@ int main()
 	Data d(15,6,2015);
 	Flechas f;
 
-vector<Personagem> personagem;
+vector<Personagem *> personagem;
 
 cout << "Simulando personagens\n\n";
 
@@ -30,11 +30,24 @@ while(true)
 {
     int i=0;
     string p1,p2;
-    cout << "Nome do personagem: ";
+	Classe: [1-Arqueiro]  [2- Espadachim]";
     cin >> p1;
-    cout << "\nClasse: ";
-    cin >> p2;
-    
+switch(p1)
+{
+	case 1:
+	{
+		personagem.push_back(new Arqueiro(20,10,Oliver,Data,Flecha));
+		personagem ++;
+	}
+	case 2:
+	{
+		personagem.push_back(new Espadachim(espadachim));
+		personagem ++;
+	}
+}
+
+Personagem *personagemPtr = dynamic_cast < Personagem *> (personagem[i]);
+
     Personagem personagem2(p1,p2);
     personagem.push_back(personagem2);
     cout << "\n\n\ncContinuar? [1-Sim] ";
@@ -49,6 +62,11 @@ for(int j=0;j<personagem.size();j++)
     personagem[i].dados();
     cout << "\n\n";
 }
-    
+
+                         
+        EmpregadodeComissaoMaisBase *derivedPtr =        
+           dynamic_cast < EmpregadoDeComissaoMaisBase * >
+              ( empregados[ i ] );  
+
 	return 0;
 }
